dec23da6bb3df81466d81ebc6ed5c22d69f9c1ad
Umesh <Umesh@CHAITANYA>
2017-11-06 22:40:19 -0600

    While entering wrong input, Enter Json Mode(OBJECT=1,STREAM=2) s, Exception in thread "main" java.util.InputMismatchException 	at java.util.Scanner.throwFor(Unknown Source) 	at java.util.Scanner.next(Unknown Source) 	at java.util.Scanner.nextInt(Unknown Source) 	at java.util.Scanner.nextInt(Unknown Source) 	at OpenWeatherAccess.scanData(OpenWeatherAccess.java:32) 	at MainCurrentTemp.main(MainCurrentTemp.java:16). 	Also wrong entry like -1, 3 for json mode and -1, s, 2 for units. These conditions were handled

--- JsonObjReader/src/OpenWeatherAccess.java
+++ JsonObjReader/src/OpenWeatherAccess.java
@@ -1,4 +1,5 @@
 import java.net.MalformedURLException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class OpenWeatherAccess {
@@ -27,29 +28,23 @@ public class OpenWeatherAccess {
 		
 		System.out.println("scanData():called");
 		//opens a scanner, keyboard
+		try {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter Json Mode(OBJECT=1,STREAM=2)");
         jsonMode = input.nextInt();
-        /*System.out.println("Enter Lat of your city");
-        lat = input.nextDouble();
-        System.out.println("Enter Lon of your city");
-        lon = input.nextDouble();*/
-        System.out.println("Enter Units 0=>Celsius, 1=>Fahrenheit");
-        unit = input.nextInt();
-        System.out.println("Enter <city>space<State>space<Country>");
-        city = input.next();
-        city = city.concat(" ").concat(input.next()).concat(" ").concat(input.next());
-        input.close();
-        
-        geoObj.setCity(city);
-                
         if(jsonMode == 1) {
         	jsonObj.setJsonMode(ParseJson.OBJECT_MODE);
         }
-        else {
+        else if (jsonMode == 2) {
         	jsonObj.setJsonMode(ParseJson.STREAM_MODE);
         }
+        else {
+        	System.out.println("Wrong units enter 1 or 2, Try again!!!");
+        	System.exit(0);
+        }
         
+        System.out.println("Enter Units 0=>Celsius, 1=>Fahrenheit");
+        unit = input.nextInt();
         if ( unit == 1) {//Fahrenheit
         	jsonObj.setUnit(true);
         }else if ( unit == 0) {//Celsius
@@ -59,6 +54,15 @@ public class OpenWeatherAccess {
         	System.out.println("Wrong units enter 0 or 1, Try again!!!");
         	System.exit(0);
         }
+        System.out.println("Enter <city>space<State>space<Country>");
+        city = input.next();
+        city = city.concat(" ").concat(input.next()).concat(" ").concat(input.next());
+        input.close();
+        geoObj.setCity(city);
+		}catch (InputMismatchException ime) {
+			System.out.println("Wrong Entry...please check the input...Try again");
+			System.exit(0);
+		}     
 	}
 	
 	private void extractGeoLoc() throws MalformedURLException{
